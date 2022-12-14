4b6dca664f6c2729b23493252a6cd9b210a1482e
clausaji <c.sanchezjimenez@qquest.nl>
2022-12-14 16:06:55 +0100

    There's a toggle function that shows or hide the punchline

--- src/Joke.jsx
+++ src/Joke.jsx
@@ -1,11 +1,19 @@
 import React from "react"
+import { useState } from "react"
 
 export default function Joke(props) {
+    const [isShown, setIsShown] = useState(false) 
+
+    function toggle(){
+        setIsShown(previousValue => !previousValue)
+    }
+
     return (
         <div>
             {/* This means basically if props setup exists print it in h3, otherwise skip*/}
-            {props.setup && <h3>Setup: {props.setup}</h3>}
-            <p>Punchline: {props.punchline}</p>
+            {props.setup &&  <h3>Setup: {props.setup}</h3>}
+            {isShown && <p>Punchline: {props.punchline}</p>}
+            <button onClick={toggle}>{isShown ? "Hide Punchline" : "Show Punchline"}</button>
             <hr />
         </div>
     )
